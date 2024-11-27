aaaf40cb65e0c471d1339e240c6d751288ab26d9
unknown <priyanshiparashar43@gmail.com>
2024-11-27 23:52:29 +0530

    java practice

--- practice/test1.java
+++ practice/test1.java
@@ -5,6 +5,7 @@ public class test1 {
         String s="Priyanshi pA4RASHAR";
         String revers=reverseStr(s);
         System.out.println(revers);
+        largestNumInArray();
     }
     public static String reverseStr(String input){
         String arr[]=input.split(" ");
@@ -18,6 +19,21 @@ public class test1 {
 
         }
         return reverseInput;
+
+
+    }
+
+    public static void largestNumInArray(){
+
+        int[] arr={1,2,4,5,6,1};
+        int l=0;
+        for(int i=0;i< arr.length;i++){
+            if(arr[i]>l){
+                l=arr[i];
+            }
+
+        }
+        System.out.println(l);
 
 
     }
